f5852483ed5eb6c407728e6e711ab9cbae2308bc
Vladimir Shevchuk <dosandkv@gmail.com>
2016-05-20 17:21:44 +0300

    init commit;

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import './styles/app.css'
+import configureStore from './store/configureStore'
+
+const store = configureStore();
+
+console.log('store', store);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
